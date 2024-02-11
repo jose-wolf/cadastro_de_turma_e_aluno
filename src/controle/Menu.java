61cf24c3e379a4bbc3a067daea5ab7c20a36dc7e
JoseERBWolf <wolf.jose89@gmail.com>
2024-02-10 23:18:27 -0300

    Criando o Menu

---
+++ src/controle/Menu.java
@@ -0,0 +1,11 @@
+package controle;
+
+public class Menu {
+
+    public static void menu(){
+        System.out.println("Digite uma das opções: ");
+        System.out.println("1- Cadastrar turma\n2- Cadastrar estudante\n3- Listar turmas\n4- Listar estudantes" +
+                "\n5- deletar turma\n6- deletar estudante\n7- Sair");
+    }
+
+}
