ee53bc9778d395db332e8d8dc7d2f7bb667c31ef
JoseERBWolf <wolf.jose89@gmail.com>
2024-02-10 23:20:42 -0300

    Classe que modela a Turma

---
+++ src/modelo/Turma.java
@@ -0,0 +1,28 @@
+package modelo;
+
+public class Turma {
+    private String codigo;
+    private String nome;
+    private int capacidade;
+
+    public Turma(String codigo, String nome, int capacidade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.capacidade = capacidade;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCapacidade() {
+        return capacidade;
+    }
+
+
+
+}
